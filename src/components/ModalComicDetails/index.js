f976619ca21b83d64f929bd3ab418f1c71a32f61
gustavokunde <gustavokunde@gmail.com>
2021-01-18 11:00:24 -0300

    corrigindo responsividade iphone

--- src/components/ModalComicDetails/index.js
+++ src/components/ModalComicDetails/index.js
@@ -25,30 +25,45 @@ function Modal({ comicDetails, closeModal }) {
               />
             </div>
             <div className="details">
-            <AiOutlineClose id="closeIcon" size={20} onClick={closeModal}/>
+              <AiOutlineClose id="closeIcon" size={20} onClick={closeModal} />
               <h2>{comicDetails.title}</h2>
               <h3>Criadores:</h3>
               <p>
-              {comicDetails.creators &&
-                comicDetails.creators.items.map((creator, index) => (
-                  index < 15 && 
-                  <span>
-                    {creator.name} - {creator.role}
-                  </span>
-                ))}
+                {comicDetails.creators &&
+                  comicDetails.creators.items.map(
+                    (creator, index) =>
+                      index < 15 && (
+                        <span>
+                          {creator.name} - {creator.role}
+                        </span>
+                      )
+                  )}
               </p>
               <h3> Custos:</h3>
               <p>
-              {comicDetails.prices &&
-                comicDetails.prices.map((price, index) => (
-                  index < 10 && 
-                  <span>
-                    {price.type ==='printPrice'?'Preço de impressão':price.type==='digitalPrice'? 'Preço de digitalização' : price.type} - {price.price.toLocaleString('en-us', { style: 'currency', currency: 'USD' })}
-                  </span>
-                ))}
+                {comicDetails.prices &&
+                  comicDetails.prices.map(
+                    (price, index) =>
+                      index < 10 && (
+                        <span>
+                          {price.type === "printPrice"
+                            ? "Preço de impressão"
+                            : price.type === "digitalPrice"
+                            ? "Preço de digitalização"
+                            : price.type}{" "}
+                          -{" "}
+                          {price.price.toLocaleString("en-us", {
+                            style: "currency",
+                            currency: "USD",
+                          })}
+                        </span>
+                      )
+                  )}
               </p>
+              <button id="closeButton" onClick={closeModal}>
+                FECHAR
+              </button>
             </div>
-          <button id="closeButton"onClick={closeModal}>FECHAR</button>
           </div>
         </div>
       )}
