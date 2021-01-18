3d63a5c39345ebcc74f04a294bc77c1f1fb5f6a7
gustavokunde <gustavokunde@gmail.com>
2021-01-18 15:15:55 -0300

    corrigindo variaveis ambiente

--- src/services/sendEmail.js
+++ src/services/sendEmail.js
@@ -1,7 +1,7 @@
 import emailjs from "emailjs-com";
 
 const email = async (data) => {
-  return emailjs.send(process.env.REACT_APP_SERVICE_ID, process.env.REACT_APP_TEMPLATE_ID, data, process.env.REACT_APP_SERVICE_ID)
+  return emailjs.send(process.env.REACT_APP_SERVICE_EMAIL, process.env.REACT_APP_TEMPLATE_ID, data, process.env.REACT_APP_USER_EMAIL_ID)
     
 };
 
